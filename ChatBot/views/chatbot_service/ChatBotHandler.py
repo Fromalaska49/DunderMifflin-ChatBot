d7b75468e9ffbb8861e4ec8a1cf9ebec91178ffd
Paco Huerta <paco.huerta1218@gmail.com>
2017-11-21 21:49:09 -0600

    removed remnants of previous FSM implementation

--- ChatBot/views/chatbot_service/ChatBotHandler.py
+++ ChatBot/views/chatbot_service/ChatBotHandler.py
@@ -1,51 +1,14 @@
 import json
-
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from ChatBot.views.misc.Constants import *
-from ChatBot.views.state_handlers.chatbot_machine.LoginStateHandler import handle_login_state
-from ChatBot.views.state_handlers.chatbot_machine.InitialStateHandler import handle_initial_state
 
 
 class ChatBotHandler(ListView):
 
     def post(self, request):
-        response_data = {}
-
-        chatbot_state = request.session.get(CHATBOT_MACHINE_STATE)
-
-        if chatbot_state == INITIAL_STATE:
-            response_data = handle_initial_state(request)
-
-        elif chatbot_state == LOGIN_STATE:
-            response_data = handle_login_state(request)
-
-        elif chatbot_state == ACCOUNT_RECOVERY_STATE:
-            pass
-
-        elif chatbot_state == INQUIRE_STATE:
-            pass
-
-        elif chatbot_state == INQUIRY_VERIFICATION_STATE:
-            pass
-
-        elif chatbot_state == AUTHENTICATION_STATE:
-            pass
-
-        elif chatbot_state == EMAIL_UPDATE_STATE:
-            pass
-
-        elif chatbot_state == NAME_UPDATE_STATE:
-            pass
-
-        elif chatbot_state == PASSWORD_UPDATE_STATE:
-            pass
-
-        return HttpResponse(json.dumps(response_data), content_type="application/json")
+        pass
 
     def get(self, request):
-        request.session[LOGIN_MACHINE_STATE] = NULL_STATE       # Temporary
-        request.session[CHATBOT_MACHINE_STATE] = INITIAL_STATE  # Temporary
-        return render(request, "chatbot.html")
+        pass
--- ChatBot/views/misc/Constants.py
+++ ChatBot/views/misc/Constants.py
@@ -1,6 +1,4 @@
 # Dictionary Key Value Constants
-LOGIN_MACHINE_STATE = 'LOGIN_STATE_MACHINE'
-CHATBOT_MACHINE_STATE = 'CHATBOT_MACHINE_STATE'
 CONFIRM_PASSWORD = 'CONFIRM_PASSWORD'
 PASS_RESET_TOKEN = 'PASS_RESET_TOKEN'
 LOGIN_ATTEMPTS = 'LOGIN_ATTEMPTS'
@@ -24,16 +22,6 @@ OIT_EMAIL = 'oitutsa@mailinator.com'
 # Toy ChatBot system email
 CHATBOT_EMAIL = 'chatbotutsa@mailinator.com'
 
-# ChatBot State Machine States
-INQUIRY_VERIFICATION_STATE = 'INQUIRY_VERIFICATION_STATE'
-ACCOUNT_RECOVERY_STATE = 'ACCOUNT_RECOVERY_STATE'
-PASSWORD_UPDATE_STATE = 'PASSWORD_UPDATE_STATE'
-AUTHENTICATION_STATE = 'AUTHENTICATION_STATE'
-EMAIL_UPDATE_STATE = 'EMAIL_UPDATE_STATE'
-NAME_UPDATE_STATE = 'INFO_UPDATE_STATE'
-INQUIRE_STATE = 'INQUIRE_STATE'
-LOGIN_STATE = 'LOGIN_STATE'
-INITIAL_STATE = None
 
 # Canned ChatBot Responses That Should Not Require DB Access
 ATTEMPTS_EXCEEDED = 'You have exceeded the amount of login attempts. This account has been locked for security reasons. ' \
@@ -43,7 +31,6 @@ ACCOUNT_IS_LOCKED = 'Your account has been locked for security reasons. Please c
                     'unlocking this account.'
 PASS_RESET_REQ_SUCCESS = 'An email with password reset instructions will be sent to the email address provided if an account' \
                      ' with that email address exists'
-INITIAL_HELP = 'Sorry, I was unable to recognize your input. You may request login or may request account recovery'
 ACCOUNT_NOT_VERIFIED = 'Your account has not yet been verified. Please verify your account before logging in'
 CREATED_USER = 'Your account has been successfully created. Please check your email to activate your account'
 COMBINATION_INVALID = 'Invalid email/password combination. try again. Please enter your email:'
@@ -52,20 +39,6 @@ LOGIN_SUCCESS = 'Login successful. What can I help you with?'
 ENTER_PASSWORD = 'Please enter your password'
 INCORRECT_PARAMETERS = 'Incorrect parameters'
 ENTER_EMAIL = 'Please enter your email'
-
-# Login State Machine States (Sub-State Machine of ChatBot Machine)
-EMAIL_STATE = 'EMAIL_STATE'
-NULL_STATE = None
-
-
-# Return Codes
-INVALID_CREDENTIALS = 'INVALID_CREDENTIALS'
-PASSWORD_REQUIRED = 'PASSWORD_REQUIRED'
-ACCOUNT_INACTIVE = 'ACCOUNT_INACTIVE'
-ACCOUNT_LOCKED = 'ACCOUNT_LOCKED'
-AUTH_SUCCESS = 'AUTH_SUCCESS'
-AMBIGUOUS = 'AMBIGUOUS'
-ERROR = 'ERROR'
 
 
 # Constants Used in General Statements
@@ -79,10 +52,12 @@ PASS_NUMBER_ERROR = 'Your password must contain at least two digits'
 PASS_LENGTH_ERROR = 'Your password must be at least 8 characters long'
 PASS_MATCH_ERROR = 'Your passwords do not match'
 
-#Email Error Messages
+
+# Email Error Messages
 EMAIL_TAKEN_ERROR = 'This email is already in use'
 EMAIL_INVALID_ERROR = 'You must provide a valid email address'
 
-#Name Error Messages
+
+# Name Error Messages
 FNAME_ERROR = 'Please enter a first name'
 LNAME_ERROR = 'Please entere a last name'
