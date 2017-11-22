17b3f09137fb5c818f62e781d8cff7e0ec287b0c
Paco Huerta <paco.huerta1218@gmail.com>
2017-11-21 22:07:30 -0600

    added timestamp fields for all user tokens

--- ChatBot/models.py
+++ ChatBot/models.py
@@ -8,6 +8,9 @@ class User(AuthUser):
     acct_active = models.BooleanField(default=False)
     is_locked = models.BooleanField(default=False)
     pass_reset_token = models.CharField(max_length=12, default="")
+    pass_reset_token_stamp = models.DateTimeField(null=True)
     acct_locked_token = models.CharField(max_length=12, default="")
+    acct_locked_token_stamp = models.DateTimeField(null=True)
     acct_verification_token = models.CharField(max_length=12, default="")
+    acct_verification_token_stamp = models.DateTimeField(null=True)
 
