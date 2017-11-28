823e8ae6ce023e0ff36877e94b83dedd3f09b5bf
Comfort Olorunlero <wio835@my.utsa.com>
2017-11-28 15:14:18 -0600

    Adding code for delete user.

--- ChatBot/views/delete_user/DeleteUser.py
+++ ChatBot/views/delete_user/DeleteUser.py
@@ -1,16 +1,25 @@
-from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.views.generic import DeleteView
-from django.http import Http404, request, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 
-from ChatBot.models import User
+
+from django.views.generic import ListView
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.shortcuts import render
+from ChatBot.views.util.EmailUtil import send_account_locked_email
+from ChatBot.views.util.AuthenticationUtil import *
+from ChatBot.views.misc.Constants import *
+from django.utils.crypto import get_random_string
+from django.utils import timezone
+import json
 
 
 class DeleteUser(DeleteView):
-    model = User
-    template_name = "delete_user.html"
-    success_url = reverse_lazy('login')
 
-    def delete(self, request, *args, **kwargs):
+    @login_required
+    def post(self, request):
         self.object = self.get_object()
         if self.object.user == request.user:
             self.object.delete()
@@ -18,3 +27,8 @@ class DeleteUser(DeleteView):
         else:
             raise Http404
             # or return HttpResponse('404_url')
+
+        # Get Request Handler
+    def get(self, request):
+        # Serve registration registration. give path relative to templates folder
+        return render(request, "delete/delete_user.html")
