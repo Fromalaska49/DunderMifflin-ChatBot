03929ebe6d293fa93d19f07344c8f40c22af6aed
Austin Mitchem <austinmitchem@outlook.com>
2017-11-28 20:32:14 -0600

    Remove old code

--- ChatBot/views/chatbot_service/ChatBotHandler.py
+++ ChatBot/views/chatbot_service/ChatBotHandler.py
@@ -1,8 +1,7 @@
 from django.views.generic import ListView
 from django.http import HttpResponse
 from django.shortcuts import render
-from ChatBot.views.chatbot_service.Intents import *
-from textblob import TextBlob
+from ChatBot.views.misc.Constants import *
 import json
 import logging
 import apiai
@@ -16,14 +15,17 @@ class ChatBotHandler(ListView):
         ai = apiai.ApiAI(API_KEY)
 
         question = request.POST[QUESTION_TEXT]
+        logger.info("Processing question: %s\n", question)
+
         request = ai.text_request()
         request.query = question
         response = json.loads(request.getresponse().read())
-        print(response)
 
         return_data = {}
         return_data[ERROR] = False
         return_data[MSG] = response['result']['fulfillment']['speech']
+
+        logger.info("Anwsering with: %s\n", return_data[MSG])
 
         return HttpResponse(json.dumps(return_data), content_type="application/json")
 
